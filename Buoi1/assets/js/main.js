e06755ee3198732522634651bb674bde626b76e7
ngoanhduy0112 <148767599+ngoanhduy0112@users.noreply.github.com>
2023-11-14 13:05:59 +0700

    up

---
+++ Buoi1/assets/js/main.js
@@ -0,0 +1,113 @@
+// HIỂN THỊ CHƯƠNG TRÌNH ĐẦU TIÊN
+// console.log("alo1234");
+
+
+
+
+
+// ĐỂ KHAI BÁO BIẾN let, const, var
+// let a = 10;
+// const b =20;
+// var c = 30;
+// IN RA GIÁ TRỊ CỦA BIẾN
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+
+
+
+// QUY TẮC ĐẶT TÊN BIẾN
+// const lopWeb38 = "FontEnd";
+// let buoiHocDauTien = "Buổi 1"
+// console.log(lopWeb38)
+// console.log(buoiHocDauTien)
+
+
+
+
+// ĐỂ KIỂM TRA 1 CÁI GIÁ TRỊ CỦA BIẾN: typeof
+// let name = "Ngo Anh Duy";
+// let age ="20";
+// let email ="example@gmail.com";
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof email);
+
+
+/*
++ PHẠM VI BIẾN
+--> Global Scope(Biến toàn cục): nằm ngài function, block
+--> Function Scope(Biến cục bộ): nằm trong function
+*/
+
+
+
+// let a =10;
+// // KHAI BÁO FUNCTION ES5
+// function hello() {
+//     // LOGIC TÍNH TOÁN
+//     console.log("hello mng");
+//     console.log(10);
+
+
+
+//     b =20;
+//     // console.log(b);   
+// }
+// // ĐỂ SỬ DỤNG FUNCTION
+// hello();
+// console.log(b);
+
+
+
+
+// let name = 'Ngo Anh Duy';
+// console.log(name);
+// let email = "abc@gmail.com";
+// console.log(email);
+
+
+
+
+// // MỘT SỐ CHÚ Ý VỚI KIỂU DỮ LIỆU STRING
+// let content= "A là học \"lớp web38\" cơ sở Tố Hữu ";
+// console.log(content);
+// let para = 'B là học sinh \'lớp web38\' cơ sở Tố Hữu';
+// console.log(para);
+// let user = "Tài khoản 'admin'";
+// console.log(user);
+
+
+// // NÊN DÙNG TRÁNH LỖI DẤU Ở TRONG KIỂU DỮ LIỆU STRING -- BACK-TICK ES6
+// let name = `Hôm nay "trời rất" lạnh. 'Đường còn tắc' nữa!`;
+// console.log(name);
+
+
+// // NỐI CHUỖI 
+// let messege = "Xin chào mọi người. "+
+// " Thời tiết hôm nay lạnh";
+// console.log(messege);
+
+
+
+// let name = "web38";
+// let skill = " FontEnd";
+// let nameSkill = name + " "+ skill;
+// console.log(nameSkill);
+
+
+// //  SỬ DỤNG BACK-TICK nối chuỗi, bỏ qua lỗi kí tự dấu nháy (đơn, kép) lồng nhau
+// let a = "Nguyen Van A";
+// let b = "Web38";
+// console.log(`Xin chào "các bạn". Mình tên là ${a} là học viên lớp ${b}`);
+
+
+// KIỂU NUMBER
+let a = -25.1;
+console.log(typeof a);
+
+
+
